0f5a75ecb82193b4ee36715bdc9495b3c8561ce3
naoya0810 <naomiti.yamada@gmail.com>
2025-05-26 14:22:23 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :profile, presence: true, length: { maximum: 200 }
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
   validates :occupation, presence: true, length: { maximum: 50 }
   validates :position, presence: true, length: { maximum: 50 }
   
